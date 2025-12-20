74c4f006611c5446fd3af1e032eeaa5b6a1ddd96
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-12-19 22:32:36 -0600

    fake backend urls

--- backend/src/middleware/csp.middleware.ts
+++ backend/src/middleware/csp.middleware.ts
@@ -6,7 +6,7 @@ import { Request, Response, NextFunction } from 'express';
 export function setCSPHeader(req: Request, res: Response, next: NextFunction) {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; font-src 'self' https://video-searcher-backend-z7yz7rqneq-uc.a.run.app; script-src 'self'; style-src 'self' 'unsafe-inline';"
+    "default-src 'self'; font-src 'self' https://video-searcher-backend-637174041158.us-central1.run.app; script-src 'self'; style-src 'self' 'unsafe-inline';"
   );
   next();
 }
